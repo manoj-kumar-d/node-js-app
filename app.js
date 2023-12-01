6389bba3b99f05cd4e8769c0b38abc9efe887c9a
Manoj Kumar D <manojkumardmk5@gmail.com>
2023-12-01 14:22:03 +0530

    Mapping a request

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button> </form></body>')
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My Page</title></head>');
