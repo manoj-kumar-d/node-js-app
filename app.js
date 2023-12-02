33071c7b04cd6d170bb4ecf66f398c068a1c90c8
Manoj Kumar D <manojkumardmk5@gmail.com>
2023-12-03 00:53:14 +0530

    Added html files

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
@@ -8,8 +10,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "error-page.html"));
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../utils/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">send</button></form>')
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
