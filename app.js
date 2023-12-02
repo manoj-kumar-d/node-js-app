1a8f8316e9d3bba10f4a800e6e1a06a1dd2a82e8
Manoj Kumar D <manojkumardmk5@gmail.com>
2023-12-02 23:58:27 +0530

    Use express Router

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log("In product page")
-    res.send('<h1>Products!</h1>')
-});
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', (req, res, next) => {
-    console.log("In the middleware 2")
-    res.send('<h1>Hello!</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 
 app.listen(3000);
