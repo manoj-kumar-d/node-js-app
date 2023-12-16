61e502ab24a5e0d6b212dca03676f058b0fa0557
Manoj Kumar D <manojkumardmk5@gmail.com>
2023-12-16 15:30:10 +0530

    Use pug templates to render all html pages

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "error-page.html"));
+    res.status(404).render("error-page", {docTitle: "Error!"});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,10 @@
-const path = require('path');
-
 const express = require('express');
-
-const rootDir = require('../utils/path');
-
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, "views", "shop.html"));
+    const products = adminData.products;
+    res.render('shop', {docTitle: "My Shop", products: products});
 });
 
 module.exports = router;
